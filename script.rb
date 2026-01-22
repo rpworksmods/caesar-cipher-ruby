de64a3a3e949d2d9caaf4c4a244864ae39e40677
rpworksmods <kyej1023@gmail.com>
2026-01-22 22:30:11 +0000

    Cipher Creation Functionality
    - Added cipher creation Functionality
    - Ensured that caps remain in their correct places
    - Will work with any string NOT containing symbols

--- script.rb
+++ script.rb
@@ -1,18 +1,36 @@
-alphabet = ("a".."z").to_a
-
+require 'pry-byebug'
 def caesar_cipher(string, shift)
   string_array = string.split("")
 
-  # Loop through array
-  #   Get the current letter's index in the alphabet array
-  #   Add the shift variable to the index to find the correct letter
-  #     IF: the new letter's index is more than the length of the alphabet array, use index 0 + remaining iterations
+  # Generate array for A-Z and a-z (to maintain caps)
+  alphabet = ("A".."Z").to_a.concat(("a".."z").to_a)
        
-  cipher_array = string_array.reduce(Hash.new(0)) do |cipher, letter|
-    alphabet_index = alphabet.index(letter) + shift
+  cipher_array = string_array.reduce(Array.new(0)) do |cipher, letter|
+
+    alphabet_index = alphabet.find_index(letter) + shift
+
+    new_index = alphabet_index + shift
+
+    #IF: the new letter's index is more than the length of the alphabet array, use index 0 + remaining iterations
     if alphabet_index > alphabet.length
-      
+      # Get the difference between index and length
+      new_index = diff(alphabet_index, alphabet.length)
     end
-    new_letter = alphabet[alphabet_index + shift]
+
+    #Get the new letter 
+    new_letter = alphabet[new_index]
+
+    #Add new letter to the cipher array
+    cipher.push(new_letter)
+
+    cipher
   end
+
+  return cipher_array.join("")
 end
+
+def diff(a, b)
+  (a - b).abs
+end
+
+puts caesar_cipher("Hello", 4)
